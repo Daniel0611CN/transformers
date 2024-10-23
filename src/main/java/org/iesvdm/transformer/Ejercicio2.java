d6e48d0e1e1659de408c0033d7cd0111f0e5b062
Dani <dclanun0611@g.educaand.es>
2024-10-23 14:36:24 +0200

    Commit

--- src/main/java/org/iesvdm/transformer/Ejercicio2.java
+++ src/main/java/org/iesvdm/transformer/Ejercicio2.java
@@ -1,6 +1,7 @@
 package org.iesvdm.transformer;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ejercicio2 {
 
@@ -8,26 +9,57 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> a = new ArrayList<>();
-        a.add(4);
-        a.add(5);
-        a.add(6);
+        Scanner sc = new Scanner(System.in);
 
-        Transformer<Integer> b = new Transformer<>() {
+//        ArrayList<Integer> a = new ArrayList<>();
+//        a.add(4);
+//        a.add(5);
+//        a.add(6);
+//
+//        Transformer<Integer> b = new Transformer<>() {
+//            @Override
+//            public Integer transform(Integer obj) {
+//                return obj*2;
+//            }
+//        };
+
+
+        ArrayList<String> b = new ArrayList<>();
+        System.out.println("\nIntroduce una cadena: ");
+        String cadena = sc.next();
+        b.add(cadena);
+
+        System.out.println("\n1. Introducir otra cadena\n2. Salir");
+        int opt = sc.nextInt();
+
+        for (int i = 0; opt == 1; i++) {
+            System.out.println("\nIntroduce una cadena: ");
+            cadena = sc.next();
+            b.add(cadena);
+            System.out.println("\n1. Introducir otra cadena\n2. Salir");
+            opt = sc.nextInt();
+        }
+
+        Transformer<String> tran = new Transformer<>() {
             @Override
-            public Integer transform(Integer obj) {
-                return obj*2;
+            public String transform(String obj) {
+                return obj;
             }
         };
 
-        ArrayList<Integer> arraytransformer = Transformers.applyDest(b, a);
+        ArrayList<String> array = Transformers.applyDest(tran, b);
+
+//        ArrayList<Integer> arraytransformer = Transformers.applyDest(b, a);
 
         System.out.println("\nArray Transformado ...");
-        for (Integer i : arraytransformer) {
+//        for (Integer i : arraytransformer) {
+//            System.out.println(i);
+//        }
+
+        for (String i : array) {
             System.out.println(i);
         }
 
+        sc.close();
     }
-
-
 }
